ac4e689b2b1a142b4e40a5b05acb87187294c102
Amit <amitabuksis8@gmail.com>
2020-04-22 12:47:31 +0300

    Updates

---
+++ precache-manifest.a9208db381d3829964b057173e033d73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "219f2b1fb762dd3ff795322cb6b80017",
+    "url": "/monsters-relodex/index.html"
+  },
+  {
+    "revision": "6818de633558fb837ad9",
+    "url": "/monsters-relodex/static/css/main.f35afc2a.chunk.css"
+  },
+  {
+    "revision": "3d2a9da766d9e0975343",
+    "url": "/monsters-relodex/static/js/2.2542a697.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-relodex/static/js/2.2542a697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6818de633558fb837ad9",
+    "url": "/monsters-relodex/static/js/main.0100bce6.chunk.js"
+  },
+  {
+    "revision": "a5d8bb23907a6335f4c5",
+    "url": "/monsters-relodex/static/js/runtime-main.32a915ae.js"
+  }
+]);
